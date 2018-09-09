3f7b598ca73c218eb263ee8fbcf20e4d27e8eaf4
Alex Ramirez <armzprz@gmail.com>
2018-09-09 11:34:28 -0500

    :white_check_mark: Add test to update object

--- __tests__/routes/applications.test.js
+++ __tests__/routes/applications.test.js
@@ -89,4 +89,49 @@ describe('routes: applications', () => {
       expect(Array.isArray(error.errors)).toBeTruthy();
     });
   });
+
+  describe('PUT /applications/:id', () => {
+    test('should update an existing Application', async () => {
+      const response = await request(server)
+        .put('/api/v1/applications/448e9c60-b3a6-11e8-825e-f1f43d9e926e')
+        .send({
+          name: 'Intergalactic DevOps',
+          tags: ['devops', 'back-end', 'node'],
+          status: 'IN_PROGRESS',
+          dateReplied: 'Sat Sep 10 2018 14:41:21 GMT-0500',
+        });
+
+      expect(response.statusCode).toBe(200);
+      expect(validateApplication(response.body).valid).toBeTruthy();
+    });
+
+    test('should return a list of format errors', async () => {
+      const response = await request(server)
+        .put('/api/v1/applications/448e9c60-b3a6-11e8-825e-f1f43d9e926e')
+        .send({
+          status: 'REJECTED',
+          newField: 'should not exist',
+        });
+
+      const error = JSON.parse(response.error.text);
+      expect(response.statusCode).toBe(400);
+      expect(Array.isArray(error.errors)).toBeTruthy();
+    });
+
+    test('should say that the application does not exist', async () => {
+      const response = await request(server)
+        .put('/api/v1/applications/548e9c60-b3a6-11e8-825e-f1f43d9e926e');
+
+      expect(response.statusCode).toBe(404);
+      expect(response.error.text).toEqual('The selected application does not exist');
+    });
+
+    test('should say that the id is not valid', async () => {
+      const response = await request(server)
+        .get('/api/v1/applications/fake-id');
+
+      expect(response.statusCode).toBe(400);
+      expect(response.error.text).toEqual('The provided id is not a uuid-v1');
+    });
+  });
 });
