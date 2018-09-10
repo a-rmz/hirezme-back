d87b329cfe2dc25c7afd9dd83d705df062a8da65
Alex Ramirez <armzprz@gmail.com>
2018-09-09 19:42:12 -0500

    :zap: Better way to interact with the db

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -7,35 +7,46 @@ require('dotenv').config();
 process.env.MONGO_DBNAME = 'hirezme-test';
 
 beforeEach(async () => {
-  await ApplicationModel.create([{
-    _id: 'ed2ce7bf-bcb6-47be-84a4-9d8ec16ea363',
-    name: 'Intergalactic DevOps',
-    url: 'https://thehub.fi/jobs/intergalactic-software-developer',
-    company: '3eee1c16-caf9-4f8d-b2cf-be6b56837a38',
-    tags: ['devops', 'backend'],
-    status: 'IN_PROGRESS',
-    dateSent: '1536427457615',
-    dateReplied: '1536427457615',
-  }, {
-    _id: '448e9c60-b3a6-11e8-825e-f1f43d9e926e',
-    name: 'Intergalactic DevOps',
-    url: 'https://thehub.fi/jobs/intergalactic-software-developer',
-    company: '3eee1c16-caf9-4f8d-b2cf-be6b56837a38',
-    tags: ['devops', 'backend'],
-    status: 'IN_PROGRESS',
-    dateSent: '1536427457615',
-    dateReplied: '1536427457615',
-  }]);
-  await CompanyModel.create({
-    _id: '3eee1c16-caf9-4f8d-b2cf-be6b56837a38',
-    name: 'Montel Intergalactic',
-    url: 'https://montel.fi',
-    location: {
-      address: 'Mikonkatu 17 A, 00100',
-      city: 'Helsinki',
-      country: 'Finland',
+  await ApplicationModel.findByIdAndUpdate(
+    'ed2ce7bf-bcb6-47be-84a4-9d8ec16ea363',
+    {
+      name: 'Intergalactic DevOps',
+      url: 'https://thehub.fi/jobs/intergalactic-software-developer',
+      company: '3eee1c16-caf9-4f8d-b2cf-be6b56837a38',
+      tags: ['devops', 'backend'],
+      status: 'IN_PROGRESS',
+      dateSent: '1536427457615',
+      dateReplied: '1536427457615',
     },
-  });
+    { upsert: true },
+  );
+  await ApplicationModel.findByIdAndUpdate(
+    '448e9c60-b3a6-11e8-825e-f1f43d9e926e',
+    {
+      name: 'Intergalactic DevOps',
+      url: 'https://thehub.fi/jobs/intergalactic-software-developer',
+      company: '3eee1c16-caf9-4f8d-b2cf-be6b56837a38',
+      tags: ['devops', 'backend'],
+      status: 'IN_PROGRESS',
+      dateSent: '1536427457615',
+      dateReplied: '1536427457615',
+    },
+    { upsert: true },
+  );
+
+  await CompanyModel.findByIdAndUpdate(
+    '3eee1c16-caf9-4f8d-b2cf-be6b56837a38',
+    {
+      name: 'Montel Intergalactic',
+      url: 'https://montel.fi',
+      location: {
+        address: 'Mikonkatu 17 A, 00100',
+        city: 'Helsinki',
+        country: 'Finland',
+      },
+    },
+    { upsert: true },
+  );
 });
 
 afterEach(async () => {
@@ -44,7 +55,14 @@ afterEach(async () => {
 });
 
 afterAll(async (done) => {
-  await mongoose.connection.close();
-  await mongoose.disconnect();
-  done();
+  try {
+    await mongoose.connection.close();
+    await mongoose.connection.collections.applications.drop();
+    await mongoose.connection.collections.companies.drop();
+    await mongoose.disconnect();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    done();
+  }
 });
