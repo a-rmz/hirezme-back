1fdb67b860ca071b08cac87bb11152c1980dc87b
Alex Ramirez <armzprz@gmail.com>
2018-09-11 19:33:13 -0500

    :bug: Allow CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 const Router = require('koa-router');
@@ -9,6 +10,7 @@ const routes = require('./routes');
 const db = require('./db/connection');
 
 const v1Router = new Router();
+app.use(cors());
 app.use(bodyParser());
 app.context.db = db;
 
